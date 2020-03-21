51159b191af524daaa9e1a179e5e36a1ba50873a
jsaurabh2794 <jsaurabh2794@gmail.com>
2020-03-21 23:14:04 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
   accoontDetailsToShowOnUI: AccountDetail[] = [];
   start: number = 0;
   end: number = 10;
+  pageSize: number = 10;
+  defaultPageNo = true;
  
   constructor(private httpClient: HttpClient) {
    this.httpClient.get<AccountDetail[]>(this.API_URL).subscribe((accountdetails: AccountDetail[]) => {
@@ -32,22 +34,40 @@ export class AppComponent {
   }
 
   goBack() {
-    if ( this.start > 0){
-      this.start = this.start - 10;
-      this.end = this.end - 10;
-      this.accoontDetailsToShowOnUI = this.accountDetails.slice(this.start , this.end);
-    }
+    if (this.end % this.pageSize !== 0) {
+      this.end = this.end - (this.end % this.pageSize);
+      this.start = this.start - this.pageSize;
+    } else {
+        if (this.start > 0 && this.start - this.pageSize > 0) {
+        this.start  = this.start - this.pageSize;
+        } else {
+        this.start = 0;
+        }
+        if (this.end > this.pageSize && this.end -this.pageSize > 10) {
+        this.end = this.end  - this.pageSize;
+        } else {
+        this.end = 10;
+      }
+  }
+    this.accoontDetailsToShowOnUI = this.accountDetails.slice(this.start , this.end);
+    console.log('Back: ' + this.start + ' ' + this.end);
   }
 
   goForward() {
-    if(this.start + 10 > this.accountDetails.length){
-      this.start = this.start + (this.end - this.start);
-      this.end = this.accountDetails.length;
-    }else if ( this.end <  this.accountDetails.length){
-      this.start = this.start + 10;
-      this.end = this.end + 10;
+    const length = this.accountDetails.length;
+    if (this.start + this.pageSize < length && this.end + this.pageSize < length) {
+      this.start = this.start + this.pageSize;
+      this.end = this.end + this.pageSize;
+    } else if (this.end + this.pageSize >= length - 1 && this.end < length - 1) {
+      if ( this.start + this.pageSize > length - 1) {
+          this.start = this.end + 1;
+      } else {
+          this.start = this.start + this.pageSize;
+      }
+      this.end = this.start + length - 1 - this.end;
     }
     this.accoontDetailsToShowOnUI = this.accountDetails.slice(this.start , this.end);
+    console.log('Next: ' + this.start + ' ' + this.end);
   }
 
 }
